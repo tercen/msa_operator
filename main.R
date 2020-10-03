45cabdcb55dd7bbe4c0f26df80cb490378b221b2
agouy <contact@agouy.com>
2020-10-03 21:44:21 +0000

    first version

--- main.R
+++ main.R
@@ -1,9 +1,20 @@
 library(tercen)
 library(dplyr)
+library(msa)
+library(Biostrings)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+ctx <- tercenCtx()
+set <- ctx$rselect(ctx$rnames[[1]])
+if(length(set[[1]]) > 500) stop("Cannot align more than 500 sequences.")
+
+AAset <- AAStringSet(set[[1]])
+
+aln <- msa(AAset)  # default clustal algo
+aln <- msaConvert(aln, type="seqinr::alignment")
+
+df_out <- data.frame(
+  .ri = 1:length(aln$seq) - 1,
+  aligned_sequence = aln$seq
+) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,23 @@
 library(tercen)
 library(dplyr)
+library(msa)
+library(Biostrings)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "a77770c3923fad0ca99b77fa8905471d")
+options("tercen.stepId"     = "dbd6c2d2-3a0f-4b1f-bcc1-016a5ee2c562")
 
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
+ctx <- tercenCtx()
+set <- ctx$rselect(ctx$rnames[[1]])
+if(length(set[[1]]) > 500) stop("Cannot align more than 500 sequences.")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+AAset <- AAStringSet(set[[1]])
+
+aln <- msa(AAset)  # default clustal algo
+aln <- msaConvert(aln, type="seqinr::alignment")
+
+df_out <- data.frame(
+  .ri = 1:length(aln$seq) - 1,
+  aligned_sequence = aln$seq
+) %>%
   ctx$addNamespace() %>%
   ctx$save()
